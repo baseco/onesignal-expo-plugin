7b2098a90046015bef9d54e12ed773372bce5ccc
mishkaloven <mishkaloven@gmail.com>
2023-03-21 11:34:47 -0700

    Adding handlers for notifications images

--- support/serviceExtensionFiles/NotificationService.m
+++ support/serviceExtensionFiles/NotificationService.m
@@ -1,29 +1,89 @@
-#import <OneSignal/OneSignal.h>
+//
+//  NotificationService.m
+//  thumbnails
+//
+//  Created by Michelle Loven on 3/16/23.
+//
 
 #import "NotificationService.h"
 
 @interface NotificationService ()
 
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
-@property (nonatomic, strong) UNNotificationRequest *receivedRequest;
 @property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+@property (nonatomic, strong) NSURLSessionDownloadTask *downloadTask;
 
 @end
 
 @implementation NotificationService
 
+
+
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    self.receivedRequest = request;
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
-    [OneSignal didReceiveNotificationExtensionRequest:self.receivedRequest
-                       withMutableNotificationContent:self.bestAttemptContent
-                                   withContentHandler:self.contentHandler];
+    
+    // Modify the notification content here...
+        [self richNotificationAttachments:self.bestAttemptContent withResponse:^(UNMutableNotificationContent * _Nullable content) {
+            self.bestAttemptContent = content;
+            self.contentHandler(self.bestAttemptContent);
+        }];
 }
 
 - (void)serviceExtensionTimeWillExpire {
-    [OneSignal serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
     self.contentHandler(self.bestAttemptContent);
 }
+
+
+- (void)richNotificationAttachments:(UNMutableNotificationContent *)originalContent withResponse:(nullable void(^)(UNMutableNotificationContent *__nullable modifiedContent))block  {
+    // For Image or Video in-app messages, we will send the media URL in the
+    // _st payload
+  
+    NSString *imageURL = originalContent.userInfo[@"aps"][@"alert"][@"image_url"];
+    NSString *videoURL = originalContent.userInfo[@"_st"][@"video_url"];
+    
+    NSURL *attachmentURL = nil;
+    if (videoURL && ![videoURL isKindOfClass:[NSNull class]]) { //Prioritize videos over image
+        attachmentURL = [NSURL URLWithString:videoURL];
+    }
+    else if (imageURL && ![imageURL isKindOfClass:[NSNull class]]) {
+        attachmentURL = [NSURL URLWithString:imageURL];
+    }
+    else {
+        block(originalContent); //Nothing to add to the push, return early.
+        return;
+    }
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    self.downloadTask = [session downloadTaskWithURL:attachmentURL completionHandler:^(NSURL *fileLocation, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            block(originalContent); //Nothing to add to the push, return early.
+            return;
+        }
+        else {
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            NSString *fileSuffix = attachmentURL.lastPathComponent;
+            
+            NSURL *typedAttachmentURL = [NSURL fileURLWithPath:[(NSString *_Nonnull)fileLocation.path stringByAppendingString:fileSuffix]];
+            [fileManager moveItemAtURL:fileLocation toURL:typedAttachmentURL error:&error];
+            
+            NSError *attachError = nil;
+            UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:typedAttachmentURL options:nil error:&attachError];
+            
+            if (attachment == nil) {
+                block(originalContent); //Nothing to add to the push, return early.
+                return;
+            }
+            
+            UNMutableNotificationContent *modifiedContent = originalContent.mutableCopy;
+            [modifiedContent setAttachments:[NSArray arrayWithObject:attachment]];
+            block(modifiedContent);
+        }
+    }];
+    [self.downloadTask resume];
+}
+
 
 @end
